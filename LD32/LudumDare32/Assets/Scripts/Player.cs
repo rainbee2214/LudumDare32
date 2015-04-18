5cd26d3a551aad3f2edd8c8b3a11dc00c41c4bf2
rainbee2214 <rainbee2214@gmail.com>
2015-04-18 03:19:12 -0300

    Working on making the player ship dynamic.

--- LD32/LudumDare32/Assets/Scripts/Player.cs
+++ LD32/LudumDare32/Assets/Scripts/Player.cs
@@ -1,28 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(ShipLoader))]
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
     //This class will control the player
+    public List<Sprite> shipColors;
+    [Range(0,3)]
+    public int currentColor;
+    int lastColor = -1;
 
     PlayerMovement movement;
+    ShipLoader shipLoader;
+
     Animator anim;
+    SpriteRenderer srenderer;
+
+
 
     void Awake()
     {
+        srenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         movement = GetComponent<PlayerMovement>();
+        shipLoader = GetComponent<ShipLoader>();
+        shipLoader.GetShipColors(ref shipColors);
     }
 
     void Update()
     {
+        if (lastColor != currentColor) ChangeShipColor();
         if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0) movement.Move(Input.GetAxisRaw("Vertical"));
         if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0) movement.Rotate(Input.GetAxisRaw("Horizontal"));
         
         if ((Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0) || (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0))
             anim.SetBool("PlayerFlying", true);
         else anim.SetBool("PlayerFlying", false);
+    }
+
+    public void ChangeShipColor()
+    {
+        srenderer.sprite = shipColors[currentColor];
+        lastColor = currentColor;
     }
 }
---
+++ LD32/LudumDare32/Assets/Scripts/ShipLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShipLoader : MonoBehaviour
+{
+    public void GetShipColors(ref List<Sprite> shipColors)
+    {
+        shipColors = new List<Sprite>();
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipBlue", typeof(Sprite)) as Sprite);
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipRed", typeof(Sprite)) as Sprite);
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipGreen", typeof(Sprite)) as Sprite);
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipOrange", typeof(Sprite)) as Sprite);
+    }
+}
