f1ef94dcd51572d14b4d95a179b2253cb5d7f62e
rainbee2214 <rainbee2214@gmail.com>
2015-04-17 22:35:45 -0300

    Initial project and assets created.

---
+++ LD32/LudumDare32/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    public bool clickToLoad = true;
+    public string level = "Menu";
+    public float delay = 1f;
+
+    float loadTime;
+
+    void Start()
+    {
+        loadTime = Time.time + delay;
+    }
+
+    void Update()
+    {
+        if (clickToLoad && Input.GetButtonDown("StartGame")) LoadLevel(level);
+        else if (!clickToLoad && Time.time > loadTime) LoadLevel(level);
+    }
+
+    public void LoadLevel(string level)
+    {
+        Application.LoadLevel(level);
+    }
+}
