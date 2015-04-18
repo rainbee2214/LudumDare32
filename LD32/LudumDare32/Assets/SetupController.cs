2fb5409a256a9b95a7eb6148b2bc6fa514c2c8ca
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-04-18 16:17:17 -0300

    Work on setup scene

--- LD32/LudumDare32/Assets/SetupController.cs
+++ LD32/LudumDare32/Assets/SetupController.cs
@@ -2,19 +2,33 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class SetupController : MonoBehaviour 
+public class SetupController : MonoBehaviour
 {
     public Button[] ship;
     public Button[] race;
     public InputField name;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void ResetShips()
+    {
+        for (int i = 0; i < ship.Length; i++)
+            ship[i].GetComponentInChildren<GameObject>().SetActive(false);
+    }
+
+    void ResetRaces()
+    {
+        for (int i = 0; i < race.Length; i++)
+            race[i].GetComponentInChildren<GameObject>().SetActive(false);   
+    }
+
+    public void ShipButtonClick(GameObject button)
+    {
+        ResetShips();
+        button.GetComponentInChildren<GameObject>().SetActive(true);
+    }
+
+    public void RaceButtonClick(GameObject button)
+    {
+        ResetRaces();
+        button.GetComponentInChildren<GameObject>().SetActive(true);
+    }
 }
