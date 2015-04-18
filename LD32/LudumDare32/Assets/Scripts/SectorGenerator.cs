362b1f7a57f7d32343608206b2c13e91bd23cac6
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-04-18 12:08:14 -0300

    Start planet generation, add resource icons

--- LD32/LudumDare32/Assets/Scripts/SectorGenerator.cs
+++ LD32/LudumDare32/Assets/Scripts/SectorGenerator.cs
@@ -4,10 +4,21 @@ using System.Collections;
 public class SectorGenerator : MonoBehaviour
 {
     //This will generate a sector of space, and populate it with planets
+    public float planetDistance = 200f;
+    public float planetVariance = 25f;
+    public int numPlanets = 10;
+    public GameObject planetPrefab;
 
     void Start()
     {
-
+        Vector3 planetPos;
+        for (int i = 0; i < numPlanets; i++)
+        {
+            planetPos = new Vector3(planetDistance * i, 0, 0);
+            planetPos.x += Random.Range(-planetVariance, +planetVariance);
+            planetPos.y += Random.Range(-planetVariance, +planetVariance);
+            Instantiate(planetPrefab, planetPos, Quaternion.identity);
+        }
     }
 
     void Update()
