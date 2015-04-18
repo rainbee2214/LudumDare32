729e9a8f80d62130baf92db4b459bb0e473e80b6
rainbee2214 <rainbee2214@gmail.com>
2015-04-18 03:28:08 -0300

    Dynamically change the color and type of ship for the player.

--- LD32/LudumDare32/Assets/Scripts/Player.cs
+++ LD32/LudumDare32/Assets/Scripts/Player.cs
@@ -8,10 +8,17 @@ using System.Collections.Generic;
 public class Player : MonoBehaviour
 {
     //This class will control the player
-    public List<Sprite> shipColors;
+
+    List<Sprite> ship1Colors;
+    List<Sprite> ship2Colors;
+    List<Sprite> ship3Colors;
+
     [Range(0,3)]
     public int currentColor;
     int lastColor = -1;
+    [Range(0, 2)]
+    public int currentShipType;
+    int lastShipType = -1;
 
     PlayerMovement movement;
     ShipLoader shipLoader;
@@ -19,20 +26,22 @@ public class Player : MonoBehaviour
     Animator anim;
     SpriteRenderer srenderer;
 
-
-
     void Awake()
     {
         srenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         movement = GetComponent<PlayerMovement>();
+
+        //Load in player choices for ships
         shipLoader = GetComponent<ShipLoader>();
-        shipLoader.GetShipColors(ref shipColors);
+        shipLoader.GetShipColors(ref ship1Colors, 1);
+        shipLoader.GetShipColors(ref ship2Colors, 2);
+        shipLoader.GetShipColors(ref ship3Colors, 3);
     }
 
     void Update()
     {
-        if (lastColor != currentColor) ChangeShipColor();
+        if (lastColor != currentColor || lastShipType != currentShipType) ChangeShipColor();
         if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0) movement.Move(Input.GetAxisRaw("Vertical"));
         if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0) movement.Rotate(Input.GetAxisRaw("Horizontal"));
         
@@ -43,7 +52,14 @@ public class Player : MonoBehaviour
 
     public void ChangeShipColor()
     {
-        srenderer.sprite = shipColors[currentColor];
+        switch (currentShipType)
+        {
+            case 0: srenderer.sprite = ship1Colors[currentColor]; break;
+            case 1: srenderer.sprite = ship2Colors[currentColor]; break;
+            case 2: srenderer.sprite = ship3Colors[currentColor]; break;
+        }
+        
         lastColor = currentColor;
+        lastShipType = currentShipType;
     }
 }
--- LD32/LudumDare32/Assets/Scripts/ShipLoader.cs
+++ LD32/LudumDare32/Assets/Scripts/ShipLoader.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 
 public class ShipLoader : MonoBehaviour
 {
-    public void GetShipColors(ref List<Sprite> shipColors)
+    public void GetShipColors(ref List<Sprite> shipColors, int shipType = 1)
     {
         shipColors = new List<Sprite>();
-        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipBlue", typeof(Sprite)) as Sprite);
-        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipRed", typeof(Sprite)) as Sprite);
-        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipGreen", typeof(Sprite)) as Sprite);
-        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShipOrange", typeof(Sprite)) as Sprite);
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShip"+shipType+"Blue", typeof(Sprite)) as Sprite);
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShip"+shipType+"Red", typeof(Sprite)) as Sprite);
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShip"+shipType+"Green", typeof(Sprite)) as Sprite);
+        shipColors.Add(Resources.Load("Assets/Sprites/Ships/playerShip"+shipType+"Orange", typeof(Sprite)) as Sprite);
     }
 }
