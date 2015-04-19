0cb3d62bfa160d7dfdd8ba55b2bc1f0e60f00b22
rainbee2214 <rainbee2214@gmail.com>
2015-04-19 17:56:15 -0300

    Working on the mini game.

---
+++ LD32/LudumDare32/Assets/Scripts/MiniGameController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MiniGameController : MonoBehaviour
+{
+    public List<GameObject> resources;
+
+    public List<GameObject> crystalPool;
+    public List<GameObject> organicsPool;
+    public List<GameObject> metalsPool;
+    public List<GameObject> peoplePool;
+    public List<GameObject> junkPool;
+
+    void Awake()
+    {
+        GetResources();
+    }
+    
+    public void StartResource(string resourceType)
+    {
+        switch (resourceType)
+        {
+            case "Crystals": break;
+            case "Organics": break;
+            case "Metals": break;
+            case "People": break;
+            case "Junk": break;
+        }
+    }
+
+    void GetResources()
+    {
+        resources = new List<GameObject>();
+        resources.Add(Resources.Load("Assets/Prefabs/Resource/Crystals", typeof(GameObject)) as GameObject);
+        resources.Add(Resources.Load("Assets/Prefabs/Resource/Organics", typeof(GameObject)) as GameObject);
+        resources.Add(Resources.Load("Assets/Prefabs/Resource/Metals", typeof(GameObject)) as GameObject);
+        resources.Add(Resources.Load("Assets/Prefabs/Resource/People", typeof(GameObject)) as GameObject);
+        resources.Add(Resources.Load("Assets/Prefabs/Resource/Junk", typeof(GameObject)) as GameObject);
+
+        crystalPool = new List<GameObject>();
+        organicsPool = new List<GameObject>();
+        metalsPool = new List<GameObject>();
+        peoplePool = new List<GameObject>();
+        junkPool = new List<GameObject>();
+
+        BuildPool(ref crystalPool, resources[0], "Crystal");
+        BuildPool(ref organicsPool, resources[1], "Organics");
+        BuildPool(ref metalsPool, resources[2], "Metals");
+        BuildPool(ref peoplePool, resources[3], "People");
+        BuildPool(ref junkPool, resources[4], "Junk");
+    }
+
+    void BuildPool(ref List<GameObject> pool, GameObject obj, string name)
+    {
+        for (int i = 0; i < 10; i++) 
+        {
+            pool.Add(Instantiate(obj));
+            pool[pool.Count - 1].name = name + (i + 1);
+            pool[pool.Count - 1].transform.SetParent(transform);
+        }
+    }
+}
--- LD32/LudumDare32/Assets/Scripts/Resource.cs
+++ LD32/LudumDare32/Assets/Scripts/Resource.cs
@@ -3,9 +3,55 @@ using System.Collections;
 
 public class Resource : MonoBehaviour
 {
+    public string stateName = "Fullsize";
+    public bool restart;
+
+    bool shrink;
+    Animator anim;
+
+    float shrinkTime;
+    float delay = 2f;
+    float delta;
+
+    void Awake()
+    {
+        delta = delay / 2f;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            restart = false;
+            Restart();
+        }
+        if (shrink && Time.time > shrinkTime)
+        {
+            shrink = false;
+            anim.SetTrigger("Shrink");
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) && !shrink)
+        {
+            shrink = true;
+            shrinkTime = Time.time + delay + Random.Range(-delta, delta);
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Invisible"))
+        {
+            anim.ResetTrigger("Shrink");
+        }
+
+
+    }
+
+    public void Restart()
+    {
+        anim.SetTrigger("Restart");
+    }
 
     void OnMouseDown()
     {
         Debug.Log("Collecting " + gameObject.name);
+        anim.SetTrigger("Clicked");
     }
 }
