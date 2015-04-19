a42b7d994f7eb7104f6f284ea231ce606b5aae92
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-04-19 20:09:20 -0300

    Setup pause

---
+++ LD32/LudumDare32/Assets/Scripts/PauseController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour
+{
+    public bool paused = false;
+    public GameObject pauseUI;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (paused)
+            {
+                Time.timeScale = 1.0f;
+                pauseUI.SetActive(false);
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                pauseUI.SetActive(true);
+            }
+            paused = !paused;
+        }
+    }
+}
