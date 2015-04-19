9a686065f0ae7d13455599a394f4238055d6d1df
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-04-19 02:09:11 -0300

    Setup close

--- LD32/LudumDare32/Assets/Scripts/SetupController.cs
+++ LD32/LudumDare32/Assets/Scripts/SetupController.cs
@@ -4,31 +4,49 @@ using UnityEngine.UI;
 
 public class SetupController : MonoBehaviour
 {
-    public Button[] ship;
-    public Button[] race;
-    public InputField name;
+    public GameObject[] shipSelect;
+    public GameObject[] raceSelect;
+    public InputField playerName;
+
+    int selectedRace = 0;
+    int selectedShipType = 0;
+    int selectedShipColor = 0;
 
     void ResetShips()
     {
-        for (int i = 0; i < ship.Length; i++)
-            ship[i].GetComponentInChildren<GameObject>().SetActive(false);
+        for (int i = 0; i < shipSelect.Length; i++)
+            shipSelect[i].SetActive(false);
     }
 
     void ResetRaces()
     {
-        for (int i = 0; i < race.Length; i++)
-            race[i].GetComponentInChildren<GameObject>().SetActive(false);   
+        for (int i = 0; i < raceSelect.Length; i++)
+            raceSelect[i].SetActive(false);
     }
 
-    public void ShipButtonClick(GameObject button)
+    //Sets the selected image on ship when clicked
+    public void ShipButtonClick(GameObject overlay)
     {
         ResetShips();
-        button.GetComponentInChildren<GameObject>().SetActive(true);
+        overlay.SetActive(true);
     }
+    
+    //Sets the type/color of ship when clicked
+    public void ShipButtonSetType(int typeIndex) { selectedShipType = typeIndex; }
+    public void ShipButtonSetColor(int colorIndex) { selectedShipColor = colorIndex; LogSelection(); }
 
-    public void RaceButtonClick(GameObject button)
+    //Sets the selected image on race when clicked
+    public void RaceButtonClick(GameObject overlay)
     {
         ResetRaces();
-        button.GetComponentInChildren<GameObject>().SetActive(true);
+        overlay.SetActive(true);
+    }
+    
+    //Sets the index of race when clicked
+    public void RaceButtonSetIndex(int typeIndex) { selectedRace = typeIndex; LogSelection(); }
+
+    void LogSelection()
+    {
+        Debug.Log("Selected Ship - Type: " + selectedShipType + " Color: " + selectedShipColor + "   Selected Race: " + selectedRace);
     }
 }
