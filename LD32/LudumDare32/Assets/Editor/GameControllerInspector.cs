4bd4f701c808c04f285aa7f29b4f58eedef250d2
rainbee2214 <rainbee2214@gmail.com>
2015-04-19 02:31:55 -0300

    Added a custom GameController inspector script.

---
+++ LD32/LudumDare32/Assets/Editor/GameControllerInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GameController))]
+public class GameControllerInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //DrawDefaultInspector();
+
+        GameController controller = (GameController)target;
+
+        //Add whatever properties you want to see here...
+        EditorGUILayout.LabelField("Property:", "" + controller.PlayerDead);
+    }
+}
+
+    
